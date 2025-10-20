4ce775728a872d93b191682964ee1de63ac346c9
Barrynetwork <barryarona2003@gmail.com>
2025-10-19 20:48:28 -0400

    creation un methode publique

--- exo1.dart
+++ exo1.dart
@@ -3,5 +3,13 @@ final r;
 final g;
 final b;
 Couleur(this.r,this.g,this.b);
+Couleur.rouge():r=255,g=0,b=0;
+Couleur.vert():r=0,g=255,b=0;
+Couleur.bleu():r=0,g=0,b=255;
+void afficherCouleur(){
+  print("R:$r,G:$g,B:$b");
+
+}
+
 
 }
