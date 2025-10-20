c85f8977b50a9d0f25ffffbba648c068fcc76c0d
Barrynetwork <barryarona2003@gmail.com>
2025-10-20 04:14:50 -0400

    redefinissions de afficherType pour Livre

--- exo4.dart
+++ exo4.dart
@@ -13,19 +13,11 @@ class Livre extends Media{
     print('Ceci est un livre: $titre par $auteur');
   }
 }
-class FILM extends Media{
+class Film extends Media{
   final int dureeMinutes;
-  FILM(super.titre,this.dureeMinutes);
+  Film(super.titre,this.dureeMinutes);
   @override
   void afficherType(){
     print('Ceci est un film: $titre en $dureeMinutes minutes');
-  }
-}
-void main(){
-  List<Media> catalogue=[];
-  catalogue.add(Livre("Leuk le lievre","Leopold Sedar Senghor"));
-  catalogue.add(FILM("Blood Diamond",110));
-  for (var media in catalogue) {
-    media.afficherType();
   }
 }
