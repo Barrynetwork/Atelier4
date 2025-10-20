da5b04631a4fd5a2fe80f44b5e3a4168afcf72b4
Barrynetwork <barryarona2003@gmail.com>
2025-10-19 21:25:52 -0400

    ajout de methode display pour l'affichage des coordonnees

--- exo2.dart
+++ exo2.dart
@@ -2,5 +2,13 @@ class Point{
   double _x,_y;
   Point(this._x,this._y);
 
+move(double dx,double dy){
+  _x+=dx;
+  _y+=dy;
+
+}
+display(){
+  print("coordonnées x:$_x, coordonnées y:$_y");
+}
 
 }
