0d0475542c11cad9d09dc51eca095199d4fc9f56
Barrynetwork <barryarona2003@gmail.com>
2025-10-24 05:01:40 -0400

    implementation de methodes  connecter et deconnecter pour serveur API

--- exo6.dart
+++ exo6.dart
@@ -3,3 +3,13 @@ abstract class Connectable{
   void connecter(String utilisateur);
   void deconnecter();
 }
+class ServeurAPI implements Connectable{
+@override
+  void connecter(String utilisateur){
+    print("ServeurAPI: connexion etablie pour $utilisateur");
+  }
+  @override
+  void deconnecter(){
+    print("ServeurAPI: deconnexion reussie");
+  }
+}
